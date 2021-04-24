4782ad86215545b520b0730260269fb8050c5749
Garret <gwiats@gmail.com>
2021-04-23 23:30:58 -0400

    all functionality done, not passing first test for some reason

--- src/ChromeBoisDomain.js
+++ src/ChromeBoisDomain.js
@@ -13,13 +13,23 @@ export default class ChromeBoisDomain extends Component {
     drawChromeBoiAtCoords(event.pageX, event.pageY)
   }
   
-  handleClick = (event) => {
-    console.log('test')
+  handleClick = () => {
+    //console.log('test')
+    toggleCycling()
   }
   /* TODO: Create an event handler which, when fired, invokes the provided
    * `toggleCycling` function with no arguments. Don't forget the click event
    * listener that should fire it!
    */
+
+  handleKeyPress = (event) => {
+    //console.log('key press')
+    if (event.key === 'a') {
+      resize('+')
+    } else if (event.key === 's') {
+      resize('-')
+    }
+  }
    
   /* TODO: Add an event listener to the `<canvas>` element to capture when a key
   /* is pressed. When a key is pressed, an event handler should invoke the
@@ -33,6 +43,7 @@ export default class ChromeBoisDomain extends Component {
       <canvas 
         onMouseMove={this.handleMouseMove}
         onClick={this.handleClick}
+        onKeyPress={this.handleKeyPress}
         width='900'
         height='600'
         tabIndex="0">
